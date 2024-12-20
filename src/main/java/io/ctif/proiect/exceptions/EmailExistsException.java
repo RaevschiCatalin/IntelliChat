a9938954010edc60ebc742b850f2f46e52f783a6
RaevschiCatalin <raevschicatalin@gmail.com>
2024-12-20 22:04:41 +0200

    file cleaning

--- src/main/java/io/ctif/proiect/exceptions/EmailExistsException.java
+++
@@ -1,7 +0,0 @@
-package io.ctif.proiect.exceptions;
-
-public class EmailExistsException extends RuntimeException {
-    public EmailExistsException(String message) {
-        super(message);
-    }
-}
